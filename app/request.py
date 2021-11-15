d90d3a27ef60db48fb64f163ef036345b2317d90
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-15 12:50:44 +0300

    Imported request

---
+++ app/request.py
@@ -0,0 +1,8 @@
+
+import request,json
+
+def get_quotes():
+    response = request.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if response.status_code == 200:
+        quote = response.json()
+        return quote
